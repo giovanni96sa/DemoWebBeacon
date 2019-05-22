fff386c53cef24819ffb68771fb0aab23847b86b
giovanni96sa <32263084+giovanni96sa@users.noreply.github.com>
2019-05-22 18:39:52 +0200

    Update ricerca.php

--- ricerca.php
+++ ricerca.php
@@ -5,28 +5,27 @@ include("accesso_db.php");
     $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
     if ($conn->connect_error) {
+		echo "{messaggio:\"Connessione fallita riprova più tardi\"}";
         die("Connection failed: " . $conn->connect_error);
     }
 	$condizione = $_GET['codice'];
     $sql = "SELECT * FROM utente WHERE Codice = $condizione";
-	echo $sql;
+	
     $result = $conn->query($sql);
 
-	//echo $result;
 	if ($result->num_rows > 0) {
 		
 		$row = $result->fetch_assoc();
-		//Acquisisco i parametri da modificare
 		$nome = $row['Nome'];
 		$cognome = $row['Cognome'];
 		$codice = $row['Codice'];
 		$gruppo = $row['Gruppo'];
 		$cabina = $row['Cabina'];
 		
-		echo "Nome:$nome Cognome:$cognome  Codice:$codice Gruppo:$gruppo Cabina:$cabina";
+		echo "{nome:$nome, cognome:$cognome,  codice:$codice, gruppo:$gruppo, cabina:$cabina}";
 		
 	} else {
-		echo "Nessun risultato per questa ricerca";
+		echo "{messaggio:\"Nessun risultato per questa ricerca\"}";
 	}
 
     $conn->close();
